3211034582f51797d149b99fe1a109a305158f80
Harun <hkolos1@etf.unsa.ba>
2022-05-04 15:55:05 +0200

    Edit profile and change password

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -49,12 +49,56 @@ router.post('/register', async ({body}, res) => {
         const {email, password, name, surname} = body;
         if(!email || !password || !name || !surname) return res.status(400).json({message: 'Empty fields'});
         if(!email || !password) return res.status(400).json({message: 'Invalid email or password'});
+        if(password.length < 5) return res.status(400).json({message: 'Password too short'});
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
 
         const user = await User.create({email, password: hash, name, surname});
         user.password = null;
         res.json({user});
+    }catch (e){
+        res.status(400).json({message: e.message || 'Something went wrong'});
+    }
+});
+
+router.post('/edit/:id', async ({body, params}, res) => {
+    try{
+        const {email, name, surname} = body;
+        if(!email && !name && !surname) return res.status(400).json({message: 'Empty fields'});
+
+        const user = await User.findById(params.id);
+
+        if(user.email)
+            user.email = email;
+        if(user.name)
+            user.name = name;
+        if(user.surname)
+            user.surname = surname;
+
+        const saved = await user.save();
+        res.json(saved);
+    }catch (e){
+        res.status(400).json({message: e.message || 'Something went wrong'});
+    }
+});
+
+router.post('/edit/password/:id', async ({body, params}, res) => {
+    try{
+        const {newPw, oldPw} = body;
+        if(!newPw || !oldPw) return res.status(400).json({message: 'Empty fields'});
+        if(newPw.length < 5) return res.status(400).json({message: 'Password too short'});
+        const user = await User.findById(params.id);
+        const passwordMatched = await bcrypt.compare(oldPw, user.password);
+        if (!passwordMatched) return res.status(400).json({message: 'Invalid email or password'});
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(newPw, salt);
+
+        user.password = hash;
+
+        const saved = await user.save();
+        saved.password = null;
+        res.json(saved);
     }catch (e){
         res.status(400).json({message: e.message || 'Something went wrong'});
     }
